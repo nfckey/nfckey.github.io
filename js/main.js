00614de149410fd38721d3139d7922604fb700b4
Mercuuury <kovshiksa@gmail.com>
2023-09-04 18:54:52 +0300

    feat: :sparkles: add the ability to zoom in on an image in the testimonial section.
    Change layout of the team section

--- js/main.js
+++ js/main.js
@@ -10,6 +10,39 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
   enableSmoothScrolling();
+
+  const imageContainer = document.querySelector(".testimonial__image-container");
+  const zoomableImage = document.querySelector(".zoomable");
+  const overlay = document.getElementById("overlay");
+  const zoomedImage = document.querySelector(".zoomed-image");
+  
+  imageContainer.addEventListener("click", () => {
+      openImage();
+  });
+  
+  overlay.addEventListener("click", () => {
+      closeImage();
+  });
+  
+  zoomedImage.addEventListener("click", () => {
+      closeImage();
+  });
+  
+  zoomedImage.addEventListener("gesturechange", (event) => {
+      zoomedImage.style.transform = `scale(${event.scale})`;
+  });
+  
+  function openImage() {
+      overlay.style.display = "flex";
+      zoomedImage.src = zoomableImage.src;
+      document.body.style.overflow = "hidden";
+  }
+  
+  function closeImage() {
+      overlay.style.display = "none";
+      zoomedImage.style.transform = "scale(1)";
+      document.body.style.overflow = "auto";
+  }
 });
 
 function toggleMenu() {
