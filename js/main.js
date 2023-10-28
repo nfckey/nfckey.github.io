83d57a4e38b5c933d55998817c6984edfbb4c66e
Mercuuury <kovshiksa@gmail.com>
2023-10-28 14:42:56 +0300

    fix: :speech_balloon: update cta form text after submission

--- js/main.js
+++ js/main.js
@@ -71,4 +71,5 @@ function onFormSubmitted() {
 function onSupFormSubmitted() {
   document.getElementById('cta-form-sup').classList.remove('cta__form');
   document.getElementById('cta-form-sup').classList.add('d-none');
+  document.getElementById('supFormText').innerHTML = 'Мы будем держать вас в курсе событий';
 }
